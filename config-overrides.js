c70e17f8070b56d2348581935a114d4f0ea01a1b
Ding Yuchao <dingyuchao@patsnap.com>
2018-06-07 12:38:21 +0800

    add polyfills

--- config-overrides.js
+++ config-overrides.js
@@ -3,9 +3,12 @@ const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin('transform-decorators-legacy', config);
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', style: true }],
+    config
+  );
   config = rewireLess.withLoaderOptions({
-    modifyVars: { '@primary-color': '#2596DB' }
+    modifyVars: { '@primary-color': '#2596DB', '@text-color': '#314659' }
   })(config, env);
 
   return config;
